97dc4f43c3e2b2b34ccd8a50111bf0ec7608a2c4
vmytsai <vlad.mytsai@gmail.com>
2023-09-14 10:30:30 +0300

    clearing the file

--- sort.py
+++ sort.py
@@ -1,7 +1,6 @@
 from pathlib import Path
 import shutil
 import sys
-import os
 
 CATEGORIES = {'images': {'categories':['JPEG', 'PNG', 'JPG', 'SVG', 'GIF', 'WEBP'], 'result':[]}, 
               'video': {'categories':['AVI', 'MP4', 'MOV', 'MKV'], 'result':[]},
@@ -15,10 +14,12 @@ known_extensions = []
 unknown_extensions = []
 
 
+# Unpacking the archive
 def unpacking(file:Path, extract_dir:Path) -> None:
     shutil.unpack_archive(file, extract_dir)
     
 
+# Move the file to a new folder with the required category
 def move(file:Path, category:str, root_dir:Path) -> None:
     target_dir = root_dir.joinpath(category)
     if not target_dir.exists():
@@ -30,20 +31,25 @@ def move(file:Path, category:str, root_dir:Path) -> None:
         file.replace(target_dir.joinpath(file.name))
     
 
+# Get file category
 def get_categories(file:Path) -> str:
     ext = file.suffix.upper().replace('.', '')
+    
     for cat, exts in CATEGORIES.items():
         if ext in exts['categories']:
             if ext not in known_extensions:
                 known_extensions.append(ext)
             exts['result'].append(file.name)
             return cat
+        
     if ext not in unknown_extensions:
         unknown_extensions.append(ext)
         CATEGORIES['other']['result'].append(file.name)
+        
     return 'other'
 
 
+# Start sorting the directory
 def sort(root_dir:Path, current_dir:Path) -> None:
     global res, known_extensions, unknown_extensions
     
@@ -57,6 +63,7 @@ def sort(root_dir:Path, current_dir:Path) -> None:
             sort(root_dir, element)
 
 
+# Displaying sorting results
 def res_choices(choice:int) -> None:
     result_list = []
     if choice == 1:
@@ -66,18 +73,21 @@ def res_choices(choice:int) -> None:
                 for file in CATEGORIES[cat]['result']:
                     print(f'|\t{file}')
                 print(f'|\n└{"":─^50}')
+                
     elif choice == 2:
         for ext in known_extensions:
             result_list.append(ext)
         print(f'\n┌{"":─^50}\n|')
         print('|\tKnown extensions: ', ', '.join(result_list), end='\n')
         print(f'|\n└{"":─^50}')
+        
     elif choice == 3:
         for ext in unknown_extensions:
             result_list.append(ext)
         print(f'\n┌{"":─^50}\n|')
         print('|\tUnknown extensions: ', ', '.join(result_list), end='\n')
         print(f'|\n└{"":─^50}')
+        
     else:
         print(f'\n┌{"":─^50}\n|')
         print('|\tUnknown option. Try again.', end='\n')
@@ -90,7 +100,6 @@ def main() -> None:
     except IndexError:
         return 'Required argument not found.\nExample: python3 main.py /Users/Mytsai/Desktop/Anything'
         
-
     if not root_dir.exists():
         return 'Folder does not exists. Try another path.'
     if not any(root_dir.iterdir()):
@@ -98,6 +107,7 @@ def main() -> None:
 
     sort(root_dir, root_dir)         
     print('\nThe work is completed :)')
+    
     while True:
         print('\nYou can get information:\n1. Display a list of files by category\n2. List of extensions that have been encountered\n3. List of unknown extensions\n0. Exit')
         choice = int(input('\nChoose from the options above: '))
